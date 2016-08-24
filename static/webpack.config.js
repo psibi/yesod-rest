58f7d836f3645bd3bd71e059c2f4723e9bc93298
Sibi Prabakaran <sibi@psibi.in>
2016-08-25 02:39:41 +0530

    Basic webpack configuration

---
+++ static/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: {
+        home: './app/jsx/home.jsx'
+    },
+    output: {
+        path: 'builds',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            }
+        ]
+    }
+};
