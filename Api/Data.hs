b69ed7c92dd52276839733b55575da80bbfc8d9c
Sibi Prabakaran <sibi@psibi.in>
2016-09-09 21:32:35 +0530

    Add ApiHandler typealias

--- Api/Data.hs
+++ Api/Data.hs
@@ -1,9 +1,15 @@
+{-#LANGUAGE RankNTypes#-}
+
 module Api.Data where
 
 import           Yesod
+import Database.Persist.Sql (SqlBackend)
+import ClassyPrelude
 
 -- Subsites have foundations just like master sites.
 data ApiSub = ApiSub
+
+type ApiHandler a = forall master. (Yesod master, YesodPersistBackend master ~ SqlBackend, YesodPersist master) => HandlerT ApiSub (HandlerT master IO) a
 
 -- We have a familiar analogue from mkYesod, with just one extra parameter.
 mkYesodSubData "ApiSub" $(parseRoutesFile "config/apiRoutes")
