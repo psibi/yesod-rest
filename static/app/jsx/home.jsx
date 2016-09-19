9e8182d221d81ecc687487a21ea2389870cedf63
Sibi Prabakaran <sibi@psibi.in>
2016-09-19 12:25:16 +0530

    Remove junk code

--- static/app/jsx/home.jsx
+++ static/app/jsx/home.jsx
@@ -3,9 +3,5 @@ import { render } from 'react-dom';
 
 const HelloWorld = () => (<div>Hello world</div>);
 
-const jim = {
-  a: 3,
-};xbkfjaxbfdk
-
 const app = document.getElementById('app');
 render(<HelloWorld />, app);
