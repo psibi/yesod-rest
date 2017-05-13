f505b16d9dd2862a2e9043572a26541679a09ea5
Sibi Prabakaran <sibi@psibi.in>
2017-05-13 16:38:57 +0530

    Add RepJSON in type signature
    Fixes #2

--- Api.hs
+++ Api.hs
@@ -1,49 +1,46 @@
-{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RankNTypes #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module Api 
-    ( module Api.Data
-    , module Api) where
+module Api
+  ( module Api.Data
+  , module Api
+  ) where
 
-import           Api.Data
-import           Yesod
+import Api.Data
+import Yesod
 import Import.NoFoundation
 
+instance (Yesod master
+         ,YesodPersistBackend master ~ SqlBackend
+         ,YesodPersist master
+         ,YesodAuth master) =>
+         YesodSubDispatch ApiSub (HandlerT master IO) where
+  yesodSubDispatch = $(mkYesodSubDispatch resourcesApiSub)
 
-instance (Yesod master, YesodPersistBackend master ~ SqlBackend, YesodPersist master, YesodAuth master) => YesodSubDispatch ApiSub (HandlerT master IO) where
-    yesodSubDispatch = $(mkYesodSubDispatch resourcesApiSub)
+getUserR :: ApiHandler RepJson
+getUserR =  return $ repJson $ object ["name" .= name, "age" .= age]
+  where
+    name = "Sibi" :: Text
+    age = 28 :: Int
 
-getUserR :: ApiHandler TypedContent
-getUserR = selectRep $ do
-                provideRep $ return $ object
-                          [ "name" .= name
-                          , "age" .= age]
-    where
-      name = "Sibi" :: Text
-      age = 28 :: Int
-
-postUserR :: ApiHandler TypedContent
+postUserR :: ApiHandler RepJson
 postUserR = do
   user <- (lift requireJsonBody)
   _ <- lift $ runDB $ insert (user :: User)
-  selectRep $ do
-              provideRep $ return $ object
-                [ "ident" .= userIdent user
-                , "password" .= userPassword user]
+  return $ repJson $ object ["ident" .= userIdent user, "password" .= userPassword user]
 
 patchUserPasswordR :: Text -> ApiHandler TypedContent
 patchUserPasswordR ident = do
-  user <- lift $ runDB $ do
-            updateWhere [UserIdent ==. ident] [UserPassword =. Nothing]
-            user <- selectFirst [UserIdent ==. ident] []
-            return user
+  user <-
+    lift $
+    runDB $
+    do updateWhere [UserIdent ==. ident] [UserPassword =. Nothing]
+       user <- selectFirst [UserIdent ==. ident] []
+       return user
   let x = invalidArgs ["User id is invalid"]
   maybe x (return . toTypedContent . toJSON) user
-
-  
-
